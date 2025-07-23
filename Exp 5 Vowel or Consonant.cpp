e8927e52af6583cf040d63ec551d99470937609a
sanskriti480 <sanskriti.ten@gmail.com>
2025-07-23 23:27:40 +0530

    Update Exp 5 Vowel or Consonant.cpp

--- Exp 5 Vowel or Consonant.cpp
+++ Exp 5 Vowel or Consonant.cpp
@@ -2,3 +2,30 @@
 //PRN-24070123054
 //Batch-A2
 //Experiment 5
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    char alphabet;
+    
+    cout << "Enter a alphabet: ";
+    cin >> alphabet;
+    
+    if(alphabet=='A' || alphabet=='E' || alphabet=='I' || alphabet=='O' || alphabet=='U' || alphabet=='a' || alphabet=='e' || alphabet=='i' || alphabet=='o' || alphabet=='u')
+    {
+        cout << "It is a vowel!!";
+    }
+    else{
+        cout << "It is a consonant!!";
+    }
+
+    return 0;
+}
+
+//Output
+
+//Program 3
+//Enter a alphabet: A
+//It is a vowel!!
